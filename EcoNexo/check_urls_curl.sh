1dc2019ec741411d6cb84eec064b1a8252ffc5af
santiagoinfantinom <santiago.infantino.moreno@gmail.com>
2026-07-06 13:15:48 +0200

    feat: move project files to root and fix repository layout

--- EcoNexo/check_urls_curl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-grep -o "https://images.unsplash.com[^\"]*" src/data/events-2026-real.ts | sort | uniq > urls.txt
-while read url; do
-  status=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-  if [ "$status" != "200" ]; then
-    echo "BROKEN ($status): $url"
-  fi
-done < urls.txt
--- EcoNexo/test-rss.js
+++
@@ -1,5 +0,0 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
-parser.parseURL('https://phys.org/rss-feed/earth-news/environment/').then(feed => {
-  console.log(feed.title, feed.items.length);
-}).catch(console.error);
